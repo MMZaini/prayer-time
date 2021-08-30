e50af4c09720b1a787ab27727c954e3e3b63f332
Mahdi Zaini <73606041+MMZaini@users.noreply.github.com>
2021-08-30 10:26:27 +0000

    testing

--- script.js
+++ script.js
@@ -1,16 +1,9 @@
-async function prayerTimes() {
-  let url = "https://mmzaini.github.io/PTCalculations/"
-  let response = await fetch(url);
-  let data = await response.json()
-  console.log(data.data.JAFARI)
-}
-
 // gets the latitude and longitude of the user
 function getLocation() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(showPosition);
   } else {
-    x.innerHTML = "Geolocation is not supported by this browser.";
+    x.innerHTML = "Geolocation is not supported by your browser.";
   }
 }
 
@@ -23,9 +16,8 @@ async function showPosition(position) {
   let url = "https://api.aladhan.com/v1/calendar?latitude=" + latitude + "&longitude=" + longitude + "&method=0&month=" + date.getMonth() + "&year=" + date.getFullYear()
   let response = await fetch(url);
   let data = await response.json()
-  document.getElementById("Day") == "Date: " + date.getDate() + date.getMonth() + date.getFullYear()
-  document.getElementById("result") == data.data
-}
+  document.getElementById("result").value == data.data
+  console.log(data.data)
 
-// asks users for location when site is loaded
-getLocation()
+  // document.getElementById("Day") == "Date: " + date.getDate() + date.getMonth() + date.getFullYear()
+}
