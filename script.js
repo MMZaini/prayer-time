540caac41437d7b768826045c72bc32433d64eb4
Mahdi Zaini <73606041+MMZaini@users.noreply.github.com>
2021-08-30 08:12:03 +0000

    testing

--- script.js
+++ script.js
@@ -10,10 +10,20 @@ async function prayerTimes() {
 
 // https://api.aladhan.com/v1/calendar?latitude=51.508515&longitude=-0.1254872&method=0&month=4&year=2017
 
-let year = new Date();
-let month = new Date();
-let latitude = ""
-let longitude = ""
 
-let url = "https://api.aladhan.com/v1/calendar?latitude=" + latitude + "&longitude=" + longitude + "&method=0&month=" + month.getMonth() + "&year=" + year.getFullYear()
-console.log(url)
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else { 
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function showPosition(position) {
+  let year = new Date();
+  let month = new Date();
+  let latitude = position.coords.latitude
+  let longitude = position.coords.longitude
+  let url = "https://api.aladhan.com/v1/calendar?latitude=" + latitude + "&longitude=" + longitude + "&method=0&month=" + month.getMonth() + "&year=" + year.getFullYear()
+  console.log(url)
+}
