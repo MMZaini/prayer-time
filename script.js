cb413cb1bc44df8946f6bf9a2ba458c4533b9a89
Mahdi Zaini <73606041+MMZaini@users.noreply.github.com>
2022-08-21 10:58:47 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,29 +1,12 @@
-// gets the latitude and longitude of the user
-function getLocation() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
-  } else {
-    window.alert("Geolocation is not supported by your browser.")
-  }
-}
-
-// gets location of user and shows result
-async function showPosition(position) {
-  let date = new Date();
-  let day = parseInt(date.getDate()) - 1
-  let latitude = position.coords.latitude
-  let longitude = position.coords.longitude
-  let url = "https://api.aladhan.com/v1/calendar?latitude=" + latitude + "&longitude=" + longitude + "&method=0&month=" + date.getMonth() + "&year=" + date.getFullYear()
+async function prayerTimes() {
+  let url = "https://api.aladhan.com/v1/timingsByCity?city=London&country=United%Kingdom&method=15&midnightMode=1"
   let response = await fetch(url);
   let data = await response.json()
-  let dayData = data.data[day]
-  document.getElementById("result").innerText = "";
-  for (const key of Object.entries(dayData['timings'])) {
-    document.getElementById("result").innerHTML += key + "<br/>"
-  }
+  let dayData = data.data.timings
+  console.log(dayData)
 
-  console.log(dayData['timings'])
-  document.getElementById("Day").innerText = "Date: " + date.toLocaleDateString()
+  document.getElementById("result").innerHTML = "Imsaak: " + dayData.Imsak + "<br>" + "Fajr: " + dayData.Fajr + "<br>" + "Sunrise: " + dayData.Sunrise + "<br>" + "Dhuhr: " + dayData.Dhuhr + "<br>" + "Sunset: " + dayData.Sunset + "<br>" + "Maghrib: " + dayData.Maghrib;
+
 }
 
-getLocation()
+prayerTimes()
